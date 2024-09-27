c3f9f8f832b3189820ecdb59ac73c27fb817483c
Herve Kouam <kouamherve01@gmail.com>
2024-09-27 15:39:00 -0400

    🚧 F01: APP > Add <Services /> with style

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ module.exports = {
       fontFamily: {
         montserrat: '"Montserrat", sans-serif',
       },
+
+      boxShadow: {
+        "custom-combined":
+          "0px 0.5px 0px 0px rgba(255, 255, 255, 0.5) inset, 0px -2px 40px 0px rgba(187, 155, 255, 0.15), 0px -2px 10px 0px rgba(233, 223, 255, 0.3)",
+      },
     },
   },
   plugins: [],
